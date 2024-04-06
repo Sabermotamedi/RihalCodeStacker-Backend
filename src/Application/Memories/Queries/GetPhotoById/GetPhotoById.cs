4bfd17482dcee730e90d5a5ba7e5ab33dfa2d123
Sabermotamedi <Sabermotamed@gmail.com>
2024-04-06 21:52:47 +0400

    Added GetMemoryPhotoById Endpoint

---
+++ src/Application/Memories/Queries/GetPhotoById/GetPhotoById.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rihal.ReelRise.Application.Common.Interfaces;
+using Rihal.ReelRise.Application.Common.Security;
+using Rihal.ReelRise.Application.Movies.Queries.GetAllMovieWithRate;
+
+namespace Rihal.ReelRise.Application.Movies.Queries;
+
+[Authorize]
+public record GetPhotoByIdQuery : IRequest<FileStream>
+{
+    public int Id { get; set; }
+}
+
+public class GetPhotoByIdHandler : IRequestHandler<GetPhotoByIdQuery, FileStream>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetPhotoByIdHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<FileStream> Handle(GetPhotoByIdQuery request, CancellationToken cancellationToken)
+    {
+        var memoryPhoto = await _context.MemoryPhoto
+                                    .AsNoTracking()
+                                    .Where(x => x.Id == request.Id)
+                                         .FirstOrDefaultAsync(cancellationToken);
+
+        if (memoryPhoto != null && !string.IsNullOrEmpty(memoryPhoto.UniqueName))
+        {
+            var filePath = Path.Combine("c:\\files", memoryPhoto.UniqueName);
+
+            return await Task.FromResult(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+        }
+        else
+        {
+            throw new FileNotFoundException($"File '{request.Id}' not found.");
+        }
+    }
+}
